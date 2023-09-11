3c17158c0799666f54c38629d89860206f8a05d5
ZzzSleepyy <aopme6936@gmail.com>
2023-09-11 21:30:50 +0800

    My first code

---
+++ zzz.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class zzz {
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        String userName = obj.nextLine();
+        System.out.print("Your Username is: " + userName);
+        obj.close();
+    }
+}
+    
