002a6242825ac6266b848a5a981fe883ea35d9e9
ZzzSleepyy <aopme6936@gmail.com>
2023-09-11 23:07:40 +0800

    coding test 2

---
+++ la.java
@@ -0,0 +1,11 @@
+
+public class la {
+    public static void main(String args[]){
+        //#declaration, assignment and initialization
+        int x; //this is a declaration
+        x = 123; //this is an assignment
+        int y = 123; //this is an initialization
+        System.out.println("This is variable x: " + x); // this is called concatenation
+    }
+}
+    
