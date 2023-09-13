cef5e91d1934a79465036873960669d6d1e20789
ZzzSleepyy <aopme6936@gmail.com>
2023-09-13 17:43:13 +0800

    my first integer to float

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        int p = 125;
+        float l = p;
+        int c = 50;
+        float q = c;
+        System.out.println(q + l);
+    }
+}
