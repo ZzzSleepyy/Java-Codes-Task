ccd7847861a2f1e8432569e69e90fbc096518aa9
Niqouu <100042636+ZzzSleepyy@users.noreply.github.com>
2023-10-13 18:57:22 +0800

    ifstatement.java

---
+++ ifstatement.java
@@ -0,0 +1,12 @@
+public class ifstatements {
+    public static void main(String args[]) {
+        int x = 0;
+        if (x == 10) {
+            System.out.println("Hi " + x);
+        }
+        else {
+            System.out.println("The value does not match.");
+        
+        }
+    }    
+}
