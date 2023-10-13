2ab5f3de399d11d5a07cf49c3a1584d08c679f39
Niqouu <100042636+ZzzSleepyy@users.noreply.github.com>
2023-10-13 23:27:38 +0800

    ifstatement.java

--- ifstatement.java
+++ ifstatement.java
@@ -10,3 +10,37 @@ public class ifstatements {
         }
     }    
 }
+
+//UPGRADE VERSION
+
+import java.util.Scanner;
+
+
+public class ifstatements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String userName = scanner.nextLine();
+
+        System.out.print("Enter age: ");
+        int age;
+
+        try {
+            age = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            age = -1;
+        }
+
+        scanner.close();
+
+        if (age > 17) {
+            System.out.println("Name: " + userName);
+            System.out.println("Age: " + age);
+        } else if (age == -1) {
+            System.out.println("String is not allowed.");
+        } else {
+            System.out.println("18 below is not allowed.");
+        }
+    }
+}
